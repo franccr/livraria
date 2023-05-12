32e072f919a70b3e377a1cf5618176addaffdedc
Cristiano França <cristiano.st.franca@gmail.com>
2023-05-12 11:41:56 -0300

    Adicionado métodos

--- src/Autor.java
+++ src/Autor.java
@@ -4,4 +4,11 @@ public class Autor {
     String email;
     String cpf;
 
+    void mostrarDetalhes(){
+        System.out.println("Mostrando detalhes do autor ");
+        System.out.println("Nome: " + nome);
+        System.out.println("E-mail: "+ email);
+        System.out.println("CPF: " + cpf);
+    }
+
 }
--- src/Livro.java
+++ src/Livro.java
@@ -7,13 +7,26 @@ public class Livro {
     String isbn;
     Autor autor;
 
+    boolean temAutor(){
+        return this.autor != null;
+    }
+
     void mostrarDetalhes() {
         System.out.println("Mostrando detalhes do livro ");
         System.out.println("Nome: " + nome);
         System.out.println("Descrição: " + descricao);
         System.out.println("Valor: " + valor);
         System.out.println("ISBN: " + isbn);
+
+        if (this.temAutor()){
+            autor.mostrarDetalhes();
+        }
+
         System.out.println("--");
+    }
+
+    public void aplicaDescontoDe(double porcentagem){
+        this.valor -= this.valor * porcentagem;
     }
 
 }
